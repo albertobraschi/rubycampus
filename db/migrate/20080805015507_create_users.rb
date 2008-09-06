441362abbe9a233784a9107dc2ce91544a53e680
Kevin Aleman <kaleman@rubycampus.org>
2008-09-06 10:40:12 +0900

    Removed first_name and last_name from User model and added name to store display name. Will use Contact model for User details since a User can also be a Contact. Example: WorkStudy student would be both a User and a Contact as would an enrolled employee of the school.

--- db/migrate/20080805015507_create_users.rb
+++ db/migrate/20080805015507_create_users.rb
@@ -38,9 +38,8 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table "users", :force => true do |t|
-      t.column :login,                     :string, :limit => 40
-      t.column :first_name,                :string, :limit => 100, :default => '', :null => true
-      t.string :last_name,                 :string, :limit => 100, :default => '', :null => true 
+      t.column :login,                     :string, :limit => 40 
+      t.string :name,                      :string, :limit => 100, :default => '', :null => true
       t.column :email,                     :string, :limit => 100
       t.column :crypted_password,          :string, :limit => 40
       t.column :salt,                      :string, :limit => 40
