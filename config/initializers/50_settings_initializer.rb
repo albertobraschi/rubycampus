bb7d0302ab012d1e6bc3a07c18dcb134c0f5e2f2
Kevin Aleman <kaleman@rubycampus.org>
2008-09-11 17:52:30 +0900

    Added indicator for demo for use in suppressing production features and presenting demo specific notices regarding simulated constituent information that looks particularly realistic though entirely fictitious.

--- config/initializers/50_settings_initializer.rb
+++ config/initializers/50_settings_initializer.rb
@@ -33,29 +33,30 @@
 # | technical reasons, the Appropriate Legal Notices must display the words "Powered   |
 # | by RubyCampus".                                                                    |
 # +------------------------------------------------------------------------------------+
-#++ 
+#++
 
 require "rubycampus/rubycampus"
 
+RubyCampus.defaults[:demo] = false
 RubyCampus.defaults[:site_name] = 'RubyCampus'
 RubyCampus.defaults[:site_protocal] = 'http://'                   # http:// or https://
-RubyCampus.defaults[:site_url] = 'rubycampus.local'               # Deployment url (eg. rubycampus.org, site.rubycampus.org) 
+RubyCampus.defaults[:site_url] = 'rubycampus.local'               # Deployment url (eg. rubycampus.org, site.rubycampus.org)
 RubyCampus.defaults[:site_from_email] = 'admin@rubycampus.local'
 RubyCampus.defaults[:site_title_default] = 'Relationship Management and Fundraising'
-RubyCampus.defaults[:site_title_login] = 'Welcome to RubyCampus'    
+RubyCampus.defaults[:site_title_login] = 'Welcome to RubyCampus'
 
 # Encrypts sensitive contact attributes stored within the database to meet and
 # exceed information storage and retrieval requirments mandated by various agencies
 # using U.S. Department of Homeland Security and National Security Agency approved
-# cryptography 
+# cryptography
 #
-# WARNING: Think very carefully about whether you really need this information 
+# WARNING: Think very carefully about whether you really need this information
 #          encrypted. Please review your organizations policy regarding the
 #          need to cryptographically protect attributes WITHIN the database
 #          itself. This means YOUR DATABASE ADMINISTRATOR CANNOT READ ENCRYPTED
 #          attributes and you will not be able to search on those attributes.
 #
-# Visit https://rubycampus.org for more information   
+# Visit https://rubycampus.org for more information
 RubyCampus.defaults[:encrypt_sensitive_attributes] = false
 
 # RubyCampus domain-wide look and feel
@@ -64,5 +65,5 @@ RubyCampus.defaults[:domain_gui_calender_theme] = 'default'
 # RubyCampus domain-wide display formats
 CalendarDateSelect.format = :hyphen_ampm
 
-# Default is Google Maps API for http://rubycampus.local       
+# Default is Google Maps API for http://rubycampus.local
 RubyCampus.defaults[:geocodable_service_api_key] = "ABQIAAAA_JjoUvqa134bquGy4vY15RRO4G5fyh1zZNzSCFmcvO7LZYRzsRT7v27fmOMDtR2bY8zj5aAhj8c8QQ"
