e79f3f931d960400f94217a86f328d89708953fd
Kevin Aleman <kaleman@rubycampus.org>
2008-09-15 10:36:30 +0900

    Adds announcements controller

---
+++ app/controllers/announcements_controller.rb
@@ -0,0 +1,125 @@
+#--                                                  /THIS FILE IS A PART OF RUBYCAMPUS/
+# +------------------------------------------------------------------------------------+
+# | RubyCampus - Relationship Management & Alumni Development Software                 |
+# +------------------------------------------------------------------------------------+
+# | Copyright (C) 2008 Kevin Aleman, RubyCampus LLC - https://rubycampus.org           |
+# +------------------------------------------------------------------------------------+
+# |                                                                                    |
+# | This program is free software; you can redistribute it and/or modify it under the  |
+# | terms of the GNU Affero General Public License version 3 as published by the Free  |
+# | Software Foundation with the addition of the following permission added to Section |
+# | 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK IN WHICH THE     |
+# | COPYRIGHT IS OWNED BY RUBYCAMPUS LLC, RUBYCAMPUS LLC DISCLAIMS THE WARRANTY OF NON |
+# | INFRINGEMENT OF THIRD PARTY RIGHTS.                                                |
+# |                                                                                    |
+# | This program is distributed in the hope that it will be useful, but WITHOUT ANY    |
+# | WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A    |
+# | PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.   |
+# |                                                                                    |
+# | You should have received a copy of the GNU Affero General Public License along     |
+# | with this program; if not, see http://www.gnu.org/licenses or write to the Free    |
+# | Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  |
+# | USA.                                                                               |
+# |                                                                                    |
+# | You can contact RubyCampus, LLC. at email address project@rubycampus.org.          |
+# |                                                                                    |
+# | The interactive user interfaces in modified source and object code versions of     |
+# | this program must display Appropriate Legal Notices, as required under Section 5   |
+# | of the GNU Affero General Public License version 3.                                |
+# |                                                                                    |
+# | In accordance with Section 7(b) of the GNU Affero General Public License version   |
+# | 3, these Appropriate Legal Notices must retain the display of the "Powered by      |
+# | RubyCampus" logo. If the display of the logo is not reasonably feasible for        |
+# | technical reasons, the Appropriate Legal Notices must display the words "Powered   |
+# | by RubyCampus".                                                                    |
+# +------------------------------------------------------------------------------------+
+#++
+
+class AnnouncementsController < ApplicationController 
+  
+  before_filter :login_required
+  
+  # GET /announcement
+  # GET /announcement.xml
+  def index
+    @announcements = Announcement.find(:all)
+
+    respond_to do |format|
+      format.html # index.haml
+      format.xml  { render :xml => @announcements }
+    end
+  end
+
+  # GET /announcement/1
+  # GET /announcement/1.xml
+  def show
+    @announcement = Announcement.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.haml
+      format.xml  { render :xml => @announcement }
+    end
+  end
+
+  # GET /announcement/new
+  # GET /announcement/new.xml
+  def new
+    @announcement = Announcement.new
+
+    respond_to do |format|
+      format.html # new.haml
+      format.xml  { render :xml => @announcement }
+    end
+  end
+
+  # GET /announcement/1/edit
+  def edit
+    @announcement = Announcement.find(params[:id])
+  end
+
+  # POST /announcement
+  # POST /announcement.xml
+  def create
+    @announcement = Announcement.new(params[:announcement])
+
+    respond_to do |format|
+      if @announcement.save
+        flash[:notice] = _("Announcement was successfully created.")
+        format.html { redirect_to(announcement_path(@announcement)) }
+        format.xml  { render :xml => @announcement, :status => :created, :location => @announcement }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @announcement.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /announcement/1
+  # PUT /announcement/1.xml
+  def update
+    @announcement = Announcement.find(params[:id])
+
+    respond_to do |format|
+      if @announcement.update_attributes(params[:announcement])
+        flash[:notice] = _("Announcement was successfully updated.")
+        format.html { redirect_to(announcement_path(@announcement)) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @announcement.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /announcement/1
+  # DELETE /announcement/1.xml
+  def destroy
+    @announcement = Announcement.find(params[:id])
+    @announcement.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(announcements_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
