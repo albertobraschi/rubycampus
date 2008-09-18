021b081b7d6276afa0c80ba99df3f9c127242f5c
Kevin Aleman <kaleman@rubycampus.org>
2008-09-18 12:40:19 +0900

    Updates sources controller

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -35,11 +35,102 @@
 # +------------------------------------------------------------------------------------+
 #++
 
-class SourcesController < ApplicationController 
+class SourcesController < ApplicationController
+  before_filter :not_logged_in_required, :only => [ :lookup ]
+  before_filter :login_required, :except => [ :lookup ]
+  before_filter :check_super_user_role, :except => [ :lookup ]
   
-  before_filter :login_required, :except => [ :index ]
+  # GET rubycampus.local/sources
+  # GET rubycampus.local/sources.xml
+  def index #:nodoc:
+    # @sources = Source.find(:all)
+    @sources = Source.search_for_all_and_paginate(params[:locate], params[:page])
+
+    respond_to do |format|
+      format.html # index.html.haml
+      # format.xml  { render :xml => @sources }
+    end
+  end
+
+  # GET rubycampus.local/sources/1
+  # GET rubycampus.local/sources/1.xml
+  def show #:nodoc:
+    @source = Source.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.haml
+      # format.xml  { render :xml => @source }
+    end
+  end
+
+  # GET rubycampus.local/sources/new
+  # GET rubycampus.local/sources/new.xml
+  def new #:nodoc:
+    @source = Source.new
+
+    respond_to do |format|
+      format.html # new.html.haml
+      # format.xml  { render :xml => @source }
+    end
+  end
+
+  # GET rubycampus.local/sources/1/edit
+  def edit #:nodoc:
+    @source = Source.find(params[:id])
+  end
+
+  # POST rubycampus.local/sources
+  # POST rubycampus.local/sources.xml
+  def create #:nodoc:
+    @source = Source.new(params[:source])
+
+    respond_to do |format|
+      if @source.save
+        flash[:notice] = _("%s was successfully created.") % _("Source")
+        if params[:create_and_new_button]
+          format.html { redirect_to new_source_url }
+        else
+          format.html { redirect_to sources_url }
+          # format.xml  { render :xml => @source, :status => :created, :location => @source }
+        end
+      else
+        format.html { render :action => "new" }
+        # format.xml  { render :xml => @source.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT rubycampus.local/sources/1
+  # PUT rubycampus.local/sources/1.xml
+  def update #:nodoc:
+    @source = Source.find(params[:id])
+
+    respond_to do |format|
+      if @source.update_attributes(params[:source])
+        flash[:notice] = _("%s was successfully updated.") % _("Source") 
+        format.html { redirect_to sources_url }
+        # format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        # format.xml  { render :xml => @source.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE rubycampus.local/sources/1
+  # DELETE rubycampus.local/sources/1.xml
+  def destroy #:nodoc:
+    @source = Source.find(params[:id])
+    @source.destroy
+
+    respond_to do |format|
+      format.html { redirect_to sources_url }
+      # format.xml  { head :ok }
+    end
+  end
   
   def lookup #:nodoc:
-    @sources = Source.find_for_auto_complete_lookup(params[:search])   
-  end                                                                                                
+    @sources = Source.find_for_auto_complete_lookup(params[:search])                            
+  end
+
 end
