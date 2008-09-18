117ab89994b49739865047c957665157d756795c
Kevin Aleman <kaleman@rubycampus.org>
2008-09-18 12:40:17 +0900

    Updates name prefixes controller

--- app/controllers/name_prefixes_controller.rb
+++ app/controllers/name_prefixes_controller.rb
@@ -36,10 +36,101 @@
 #++
 
 class NamePrefixesController < ApplicationController
+  before_filter :not_logged_in_required, :only => [ :lookup ]
+  before_filter :login_required, :except => [ :lookup ]
+  before_filter :check_super_user_role, :except => [ :lookup ]
   
-  before_filter :login_required, :except => [ :index ]
+  # GET rubycampus.local/name_prefixes
+  # GET rubycampus.local/name_prefixes.xml
+  def index #:nodoc:
+    # @name_prefixes = NamePrefix.find(:all)
+    @name_prefixes = NamePrefix.search_for_all_and_paginate(params[:locate], params[:page])
+
+    respond_to do |format|
+      format.html # index.html.haml
+      # format.xml  { render :xml => @name_prefixes }
+    end
+  end
+
+  # GET rubycampus.local/name_prefixes/1
+  # GET rubycampus.local/name_prefixes/1.xml
+  def show #:nodoc:
+    @name_prefix = NamePrefix.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.haml
+      # format.xml  { render :xml => @name_prefix }
+    end
+  end
+
+  # GET rubycampus.local/name_prefixes/new
+  # GET rubycampus.local/name_prefixes/new.xml
+  def new #:nodoc:
+    @name_prefix = NamePrefix.new
+
+    respond_to do |format|
+      format.html # new.html.haml
+      # format.xml  { render :xml => @name_prefix }
+    end
+  end
+
+  # GET rubycampus.local/name_prefixes/1/edit
+  def edit #:nodoc:
+    @name_prefix = NamePrefix.find(params[:id])
+  end
+
+  # POST rubycampus.local/name_prefixes
+  # POST rubycampus.local/name_prefixes.xml
+  def create #:nodoc:
+    @name_prefix = NamePrefix.new(params[:name_prefix])
+
+    respond_to do |format|
+      if @name_prefix.save
+        flash[:notice] = _("%s was successfully created.") % _("NamePrefix")
+        if params[:create_and_new_button]
+          format.html { redirect_to new_name_prefix_url }
+        else
+          format.html { redirect_to name_prefixes_url }
+          # format.xml  { render :xml => @name_prefix, :status => :created, :location => @name_prefix }
+        end
+      else
+        format.html { render :action => "new" }
+        # format.xml  { render :xml => @name_prefix.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT rubycampus.local/name_prefixes/1
+  # PUT rubycampus.local/name_prefixes/1.xml
+  def update #:nodoc:
+    @name_prefix = NamePrefix.find(params[:id])
+
+    respond_to do |format|
+      if @name_prefix.update_attributes(params[:name_prefix])
+        flash[:notice] = _("%s was successfully updated.") % _("NamePrefix") 
+        format.html { redirect_to name_prefixes_url }
+        # format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        # format.xml  { render :xml => @name_prefix.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE rubycampus.local/name_prefixes/1
+  # DELETE rubycampus.local/name_prefixes/1.xml
+  def destroy #:nodoc:
+    @name_prefix = NamePrefix.find(params[:id])
+    @name_prefix.destroy
+
+    respond_to do |format|
+      format.html { redirect_to name_prefixes_url }
+      # format.xml  { head :ok }
+    end
+  end
   
   def lookup #:nodoc:
-    @name_prefixes = NamePrefix.find_for_auto_complete_lookup(params[:search])   
-  end                                                                                     
+    @name_prefixes = NamePrefix.find_for_auto_complete_lookup(params[:search])                            
+  end
+
 end
