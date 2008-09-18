4cf2a529da7176bc8b059da00e16b9de0c642bee
Kevin Aleman <kaleman@rubycampus.org>
2008-09-18 12:40:16 +0900

    Updates the genders controller

--- app/controllers/genders_controller.rb
+++ app/controllers/genders_controller.rb
@@ -36,10 +36,101 @@
 #++
 
 class GendersController < ApplicationController
+  before_filter :not_logged_in_required, :only => [ :lookup ]
+  before_filter :login_required, :except => [ :lookup ]
+  before_filter :check_super_user_role, :except => [ :lookup ]
   
-  before_filter :login_required, :except => [ :index ]   
-    
+  # GET rubycampus.local/genders
+  # GET rubycampus.local/genders.xml
+  def index #:nodoc:
+    # @genders = Gender.find(:all)
+    @genders = Gender.search_for_all_and_paginate(params[:locate], params[:page])
+
+    respond_to do |format|
+      format.html # index.html.haml
+      # format.xml  { render :xml => @genders }
+    end
+  end
+
+  # GET rubycampus.local/genders/1
+  # GET rubycampus.local/genders/1.xml
+  def show #:nodoc:
+    @gender = Gender.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.haml
+      # format.xml  { render :xml => @gender }
+    end
+  end
+
+  # GET rubycampus.local/genders/new
+  # GET rubycampus.local/genders/new.xml
+  def new #:nodoc:
+    @gender = Gender.new
+
+    respond_to do |format|
+      format.html # new.html.haml
+      # format.xml  { render :xml => @gender }
+    end
+  end
+
+  # GET rubycampus.local/genders/1/edit
+  def edit #:nodoc:
+    @gender = Gender.find(params[:id])
+  end
+
+  # POST rubycampus.local/genders
+  # POST rubycampus.local/genders.xml
+  def create #:nodoc:
+    @gender = Gender.new(params[:gender])
+
+    respond_to do |format|
+      if @gender.save
+        flash[:notice] = _("%s was successfully created.") % _("Gender")
+        if params[:create_and_new_button]
+          format.html { redirect_to new_gender_url }
+        else
+          format.html { redirect_to genders_url }
+          # format.xml  { render :xml => @gender, :status => :created, :location => @gender }
+        end
+      else
+        format.html { render :action => "new" }
+        # format.xml  { render :xml => @gender.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT rubycampus.local/genders/1
+  # PUT rubycampus.local/genders/1.xml
+  def update #:nodoc:
+    @gender = Gender.find(params[:id])
+
+    respond_to do |format|
+      if @gender.update_attributes(params[:gender])
+        flash[:notice] = _("%s was successfully updated.") % _("Gender") 
+        format.html { redirect_to genders_url }
+        # format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        # format.xml  { render :xml => @gender.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE rubycampus.local/genders/1
+  # DELETE rubycampus.local/genders/1.xml
+  def destroy #:nodoc:
+    @gender = Gender.find(params[:id])
+    @gender.destroy
+
+    respond_to do |format|
+      format.html { redirect_to genders_url }
+      # format.xml  { head :ok }
+    end
+  end
+  
   def lookup #:nodoc:
-    @genders = Gender.find_for_auto_complete_lookup(params[:search])
-  end                                                                                                   
+    @genders = Gender.find_for_auto_complete_lookup(params[:search])                            
+  end
+
 end
