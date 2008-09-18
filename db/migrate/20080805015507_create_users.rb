d1a804efd01e182d9c4e4f74012a896bb6f3e783
Kevin Aleman <kaleman@rubycampus.org>
2008-09-18 19:19:35 +0900

    Adds language to user migration, defaults to English

--- db/migrate/20080805015507_create_users.rb
+++ db/migrate/20080805015507_create_users.rb
@@ -37,30 +37,31 @@
 
 class CreateUsers < ActiveRecord::Migration
   def self.up
-    create_table "users", :force => true do |t|
-      t.column :login,                     :string, :limit => 40 
-      t.string :name,                      :string, :limit => 100, :default => '', :null => true
-      t.column :email,                     :string, :limit => 100
-      t.column :crypted_password,          :string, :limit => 40
-      t.column :salt,                      :string, :limit => 40
-      t.column :created_at,                :datetime
-      t.column :updated_at,                :datetime
-      t.column :remember_token,            :string, :limit => 40
-      t.column :remember_token_expires_at, :datetime
-      t.column :activation_code,           :string, :limit => 40
-      t.column :activated_at,              :datetime  
-      t.string :password_reset_code,       :limit => 40
-      t.boolean :enabled,                  :default => true
-      t.integer :updated_by   
-      t.column :state,                     :string, :null => :no, :default => 'passive'
-      t.column :deleted_at,                :datetime
-      t.boolean :is_admin,                 :default => false     
-      t.string :time_zone   
+    create_table :users do |t|
+      t.string     :login,                     :limit => 40 
+      t.string     :name,                      :limit => 100, :default => '', :null => true
+      t.string     :email,                     :limit => 100
+      t.string     :crypted_password,          :limit => 40
+      t.string     :salt,                      :limit => 40
+      t.datetime   :created_at
+      t.datetime   :updated_at
+      t.string     :remember_token,            :limit => 40
+      t.datetime   :remember_token_expires_at
+      t.string     :activation_code,           :limit => 40
+      t.datetime   :activated_at  
+      t.string     :password_reset_code,       :limit => 40
+      t.boolean    :enabled,                   :default => true
+      t.integer    :updated_by   
+      t.string     :state,                     :null => :no, :default => 'passive'
+      t.datetime   :deleted_at
+      t.boolean    :is_admin,                  :default => false     
+      t.string     :time_zone
+      t.string     :language,                  :limit => 5,  :default => 'en', :null => false   
     end
     add_index :users, :login, :unique => true
   end
 
   def self.down
-    drop_table "users"
+    drop_table :users
   end
 end
