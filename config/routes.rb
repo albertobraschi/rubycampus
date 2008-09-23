75e0d1a081ef82abf19a5cd5f429733a7e382425
Kevin Aleman <kaleman@rubycampus.org>
2008-09-24 05:33:17 +0900

    Adds group types

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :ethnicities, :collection => { :lookup => :get }
   map.resources :genders, :collection => { :lookup => :get }
   map.resources :greetings, :collection => { :lookup => :get }
+  map.resources :group_types, :collection => { :lookup => :get }
   map.resources :head_of_households, :collection => { :lookup => :get }
   map.resources :location_types, :collection => { :lookup => :get }
   map.resources :marital_statuses, :collection => { :lookup => :get }
