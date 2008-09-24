e317d32189890b4b150e9f9ed895e68cb2df0516
Kevin Aleman <kaleman@rubycampus.org>
2008-09-24 15:52:29 +0900

    Standardizes and cleans up migration

--- db/migrate/20080815204504_create_education_levels.rb
+++ db/migrate/20080815204504_create_education_levels.rb
@@ -38,10 +38,10 @@
 class CreateEducationLevels < ActiveRecord::Migration
   def self.up
     create_table :education_levels do |t|
-      t.string :name
-      t.integer :position
-      t.boolean :is_default, :default => false      
-      t.boolean :is_enabled, :default => true
+      t.string     :name, :null => false
+      t.integer    :position
+      t.boolean    :is_default, :default => false      
+      t.boolean    :is_enabled, :default => true
       t.timestamps
     end 
     require 'active_record/fixtures'
