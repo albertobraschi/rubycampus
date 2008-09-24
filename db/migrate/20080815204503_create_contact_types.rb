a2fd2633c1353aa5b46ff0b9b54fc55631b24aee
Kevin Aleman <kaleman@rubycampus.org>
2008-09-24 15:52:27 +0900

    Standardizes and cleans up migration

--- db/migrate/20080815204503_create_contact_types.rb
+++ db/migrate/20080815204503_create_contact_types.rb
@@ -38,11 +38,11 @@
 class CreateContactTypes < ActiveRecord::Migration
   def self.up
     create_table :contact_types do |t|
-      t.string  :name,            :null => false
-      t.integer :position
-      t.boolean :is_default, :default => false      
-      t.boolean :is_reserved, :default => false
-      t.boolean :is_enabled, :default => true
+      t.string     :name, :null => false
+      t.integer    :position
+      t.boolean    :is_default, :default => false      
+      t.boolean    :is_reserved, :default => false
+      t.boolean    :is_enabled, :default => true
       t.timestamps
     end     
     require 'active_record/fixtures'
