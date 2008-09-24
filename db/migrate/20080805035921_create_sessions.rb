b5ca8023b50ff9e4965381e671b9e1979bfc01b0
Kevin Aleman <kaleman@rubycampus.org>
2008-09-24 15:52:15 +0900

    Standardizes and cleans up migration

--- db/migrate/20080805035921_create_sessions.rb
+++ db/migrate/20080805035921_create_sessions.rb
@@ -38,8 +38,8 @@
 class CreateSessions < ActiveRecord::Migration
   def self.up
     create_table :sessions do |t|
-      t.string :session_id, :null => false
-      t.text :data
+      t.string     :session_id, :null => false
+      t.text       :data
       t.timestamps
     end
 
