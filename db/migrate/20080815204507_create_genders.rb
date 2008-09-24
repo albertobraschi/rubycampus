3fbae1f940cfee169591d14ef2f5719b1fa391b3
Kevin Aleman <kaleman@rubycampus.org>
2008-09-24 15:52:39 +0900

    Standardizes and cleans up migration

--- db/migrate/20080815204507_create_genders.rb
+++ db/migrate/20080815204507_create_genders.rb
@@ -38,11 +38,11 @@
 class CreateGenders < ActiveRecord::Migration
   def self.up
     create_table :genders do |t|
-      t.string :name
-      t.integer :position
-      t.boolean :is_default, :default => false      
-      t.boolean :is_reserved, :default => false
-      t.boolean :is_enabled, :default => true
+      t.string     :name, :null => false
+      t.integer    :position
+      t.boolean    :is_default, :default => false      
+      t.boolean    :is_reserved, :default => false
+      t.boolean    :is_enabled, :default => true
       t.timestamps
     end  
     require 'active_record/fixtures'
