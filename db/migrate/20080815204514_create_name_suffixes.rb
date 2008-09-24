8c40bdcc4a43e2b41163fa8decfd415db89002ba
Kevin Aleman <kaleman@rubycampus.org>
2008-09-24 15:52:54 +0900

    Standardizes and cleans up migration

--- db/migrate/20080815204514_create_name_suffixes.rb
+++ db/migrate/20080815204514_create_name_suffixes.rb
@@ -38,11 +38,11 @@
 class CreateNameSuffixes < ActiveRecord::Migration
   def self.up
     create_table :name_suffixes do |t|
-      t.string :name
-      t.integer :position  
-      t.boolean :is_default, :default => false      
-      t.boolean :is_reserved, :default => false
-      t.boolean :is_enabled, :default => true
+      t.string     :name, :null => false
+      t.integer    :position  
+      t.boolean    :is_default, :default => false
+      t.boolean    :is_reserved, :default => false
+      t.boolean    :is_enabled, :default => true
       t.timestamps
     end
     require 'active_record/fixtures'
