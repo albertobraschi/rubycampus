26b9aeeba9650a2e41784bd05f5fb8d4c1fcb8df
Kevin Aleman <kaleman@rubycampus.org>
2008-09-24 15:52:53 +0900

    Standardizes and cleans up migration

--- db/migrate/20080815204512_create_mobile_providers.rb
+++ db/migrate/20080815204512_create_mobile_providers.rb
@@ -38,10 +38,10 @@
 class CreateMobileProviders < ActiveRecord::Migration
   def self.up
     create_table :mobile_providers do |t|
-      t.string   :name
-      t.integer  :position
-      t.boolean  :is_default, :default => false      
-      t.boolean  :is_enabled, :default => true
+      t.string     :name, :null => false
+      t.integer    :position
+      t.boolean    :is_default, :default => false      
+      t.boolean    :is_enabled, :default => true
       t.timestamps
     end
     require 'active_record/fixtures'
