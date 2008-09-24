27f81541ad98c20ac895280b8df963f4d19a5cd6
Kevin Aleman <kaleman@rubycampus.org>
2008-09-24 15:52:39 +0900

    Standardizes and cleans up migration

--- db/migrate/20080815204508_create_greetings.rb
+++ db/migrate/20080815204508_create_greetings.rb
@@ -38,10 +38,10 @@
 class CreateGreetings < ActiveRecord::Migration
   def self.up
     create_table :greetings do |t|
-      t.string :name
-      t.integer :position
-      t.boolean :is_default, :default => false      
-      t.boolean :is_enabled, :default => true
+      t.string     :name, :null => false
+      t.integer    :position
+      t.boolean    :is_default, :default => false      
+      t.boolean    :is_enabled, :default => true
       t.timestamps
     end 
     require 'active_record/fixtures'
