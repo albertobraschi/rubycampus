8c2e84dd8ee329e0889f369be74e38bc231669fa
Adam Bailey <l.adam.bailey@gmail.com>
2008-10-02 22:14:09 +0900

    Adds application helpers for AJAX sorting and using the label of <th> tags for sorting by turning them into state sensitive hyperlinks.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -185,5 +185,47 @@ module ApplicationHelper
       end
       out << "</ul>"                  
   end
+  
+  def pagination_links_remote(paginator)
+    page_options = {:window_size => 1}
+    pagination_links_each(paginator, page_options) do |n|
+      options = {
+        :url => {:action => 'index', :params => params.merge({:page => n})},
+        :update => 'table',
+        :before => "Element.show('spinner')",
+        :success => "Element.hide('spinner')"
+      }
+      html_options = {:href => url_for(:action => 'index', :params => params.merge({:page => n}))}
+      link_to_remote(n.to_s, options, html_options)
+    end
+  end
+  
+  #
+  # Adds contextual sorting class to tag
+  #
+  def sort_th_class_helper(param)
+    result = 'class="sortup"' if params[:sort] == param
+    result = 'class="sortdown"' if params[:sort] == param + "_reverse"
+    return result
+  end
+  
+  #
+  # Generates link for sorting on provided text
+  #
+  def sort_link_helper(text, param)
+    key = param
+    key += "_reverse" if params[:sort] == param
+    options = {
+        :url => {:action => 'index', :params => params.merge({:sort => key, :page => nil})},
+        :update => 'table',
+        :before => "Element.show('spinner')",
+        :success => "Element.hide('spinner')"
+    }
+    html_options = {
+      :title => _("Sort by this field"),
+      :href => url_for(:action => 'index', :params => params.merge({:sort => key, :page => nil}))
+    }
+    link_to_remote(text, options, html_options)
+  end
 
 end
