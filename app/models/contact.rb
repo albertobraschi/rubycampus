20ff66bb731dbd99ab069854e418bafbd5966d00
Kevin Aleman <kaleman@rubycampus.org>
2008-10-04 21:26:01 +0900

    Removes encrypt_sensitive_attributes from settings.yml

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -42,7 +42,7 @@ class Contact < ActiveRecord::Base
   untranslate_all
  
   # Encrypts sensitive information INSIDE database
-  if Setting.encrypt_sensitive_attributes?
+  if AppConfig.encrypt_sensitive_attributes?
     acts_as_secure :crypto_provider => MasterCryptoProvider
   end
   
