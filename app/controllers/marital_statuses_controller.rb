d3b7ea822e77496ada82571edd656caad47eb17b
Adam Bailey <abailey@rubycampus.org>
2008-10-03 21:01:23 -0400

    Issue #92, adds sorting, search and pagination to index action

--- app/controllers/marital_statuses_controller.rb
+++ app/controllers/marital_statuses_controller.rb
@@ -42,12 +42,22 @@ class MaritalStatusesController < ApplicationController
   # GET rubycampus.local/marital_statuses
   # GET rubycampus.local/marital_statuses.xml
   def index #:nodoc:
-    # @marital_statuses = MaritalStatus.find(:all)
-    @marital_statuses = MaritalStatus.search_for_all_and_paginate(params[:locate], params[:page])
+    sort = case params['sort']
+           when "name"  then "name"
+           when "name_reverse"  then "name DESC"
+           when "is_default"  then "is_default"
+           when "is_default_reverse"  then "is_default DESC"
+           when "is_enabled"  then "is_default"
+           when "is_enabled_reverse"  then "is_enabled DESC"
+           end
 
-    respond_to do |format|
-      format.html # index.html.haml
-      # format.xml  { render :xml => @marital_statuses }
+    conditions = ["name LIKE ?", "%#{params[:query]}%"] unless params[:query].nil?
+
+    @total = MaritalStatus.count(:conditions => conditions)
+    @marital_statuses_pages, @marital_statuses = paginate :marital_statuses, :order => sort, :conditions => conditions, :per_page => AppConfig.rows_per_page
+
+    if request.xml_http_request?
+      render :partial => "marital_statuses", :layout => false
     end
   end
 
