dbd909a90c4e8ff6287fd06c5f420014f84c5c33
Adam Bailey <abailey@rubycampus.org>
2008-10-03 20:24:48 -0400

    Issue #90, adds sorting, search and pagination to index action

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -42,12 +42,22 @@ class GreetingsController < ApplicationController
   # GET rubycampus.local/greetings
   # GET rubycampus.local/greetings.xml
   def index #:nodoc:
-    # @greetings = Greeting.find(:all)
-    @greetings = Greeting.search_for_all_and_paginate(params[:locate], params[:page])
+    sort = case params['sort']
+           when "name"  then "name"
+           when "name_reverse"  then "name DESC"
+           when "is_default"  then "is_default"
+           when "is_default_reverse"  then "is_default DESC"
+           when "is_enabled"  then "is_default"
+           when "is_enabled_reverse"  then "is_enabled DESC"
+           end
 
-    respond_to do |format|
-      format.html # index.html.haml
-      # format.xml  { render :xml => @greetings }
+    conditions = ["name LIKE ?", "%#{params[:query]}%"] unless params[:query].nil?
+
+    @total = AcademicLevel.count(:conditions => conditions)
+    @greetings_pages, @greetings = paginate :greetings, :order => sort, :conditions => conditions, :per_page => AppConfig.rows_per_page
+
+    if request.xml_http_request?
+      render :partial => "greetings", :layout => false
     end
   end
 
