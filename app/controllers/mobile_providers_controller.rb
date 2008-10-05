4531ad9717399618268df71eda77c83010742aac
Adam Bailey <abailey@rubycampus.org>
2008-10-04 21:24:06 -0400

    Issue #99, adds sorting, search and pagination to index action

--- app/controllers/mobile_providers_controller.rb
+++ app/controllers/mobile_providers_controller.rb
@@ -42,12 +42,22 @@ class MobileProvidersController < ApplicationController
   # GET rubycampus.local/mobile_providers
   # GET rubycampus.local/mobile_providers.xml
   def index #:nodoc:
-    # @mobile_providers = MobileProvider.find(:all)
-    @mobile_providers = MobileProvider.search_for_all_and_paginate(params[:locate], params[:page])
+    sort = case params['sort']
+           when "name"  then "name"
+           when "name_reverse"  then "name DESC"
+           when "is_default"  then "is_default"
+           when "is_default_reverse"  then "is_default DESC"
+           when "is_enabled"  then "is_default"
+           when "is_enabled_reverse"  then "is_enabled DESC"
+           end
 
-    respond_to do |format|
-      format.html # index.html.haml
-      # format.xml  { render :xml => @mobile_providers }
+    conditions = ["name LIKE ?", "%#{params[:query]}%"] unless params[:query].nil?
+
+    @total = MobileProvider.count(:conditions => conditions)
+    @mobile_providers_pages, @mobile_providers = paginate :mobile_providers, :order => sort, :conditions => conditions, :per_page => AppConfig.rows_per_page
+
+    if request.xml_http_request?
+      render :partial => "mobile_providers", :layout => false
     end
   end
 
