f05bdf9342490c64c97e8e09480a8208ff16d530
Adam Bailey <abailey@rubycampus.org>
2008-10-04 21:38:43 -0400

    Issue #100, Adds :index => :get to resource collection for :name_suffixes

--- config/routes.rb
+++ config/routes.rb
@@ -72,7 +72,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :messaging_providers, :collection => { :lookup => :get }
   map.resources :mobile_providers, :collection => { :lookup => :get, :index => :get }
   map.resources :name_prefixes, :collection => { :lookup => :get }
-  map.resources :name_suffixes, :collection => { :lookup => :get }
+  map.resources :name_suffixes, :collection => { :lookup => :get, :index => :get }
   map.resources :phone_types, :collection => { :lookup => :get }   
   map.resources :programs, :collection => { :lookup => :get }
   map.resources :regions, :collection => { :lookup => :get }
