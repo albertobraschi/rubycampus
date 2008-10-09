f44059fe9c96e62a67a4565b3dfaeb798a9f0e6f
Kevin Aleman <kaleman@rubycampus.org>
2008-10-10 00:22:31 +0900

    References #110 adds acts_as_revision block and an additional class for accessing model revisions

--- app/models/ethnicity.rb
+++ app/models/ethnicity.rb
@@ -42,6 +42,10 @@ class Ethnicity < ActiveRecord::Base
    
   has_many :contacts
   
+  acts_as_revisable do
+    revision_class_name "EthnicityRevision"
+  end
+  
   # begin Validations
     validates_presence_of :name
   # ends Validations
@@ -62,6 +66,9 @@ class Ethnicity < ActiveRecord::Base
   NAMES_KEYS = self.find(:all).map do |s| 
   [s.name, s.id] 
   end
+end
+class EthnicityRevision < ActiveRecord::Base
+  acts_as_revision :revisable_class_name => "Ethnicity"
 end
 
 # == Schema Information
