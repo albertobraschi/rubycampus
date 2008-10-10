d99ef728b0aa5b9d79e9f99ee791341e4d8850b2
Kevin Aleman <kaleman@rubycampus.org>
2008-10-10 11:59:19 +0900

    Adds specification for status model

---
+++ spec/models/status_spec.rb
@@ -0,0 +1,70 @@
+#--                                                  /THIS FILE IS A PART OF RUBYCAMPUS/
+# +------------------------------------------------------------------------------------+
+# | RubyCampus - Student & Alumni Relationship Management Software                     |
+# +------------------------------------------------------------------------------------+
+# | Copyright (C) 2008 Kevin Aleman, RubyCampus LLC - https://rubycampus.org           |
+# +------------------------------------------------------------------------------------+
+# |                                                                                    |
+# | This status is free software; you can redistribute it and/or modify it under the  |
+# | terms of the GNU Affero General Public License version 3 as published by the Free  |
+# | Software Foundation with the addition of the following permission added to Section |
+# | 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK IN WHICH THE     |
+# | COPYRIGHT IS OWNED BY RUBYCAMPUS LLC, RUBYCAMPUS LLC DISCLAIMS THE WARRANTY OF NON |
+# | INFRINGEMENT OF THIRD PARTY RIGHTS.                                                |
+# |                                                                                    |
+# | This status is distributed in the hope that it will be useful, but WITHOUT ANY    |
+# | WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A    |
+# | PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.   |
+# |                                                                                    |
+# | You should have received a copy of the GNU Affero General Public License along     |
+# | with this status; if not, see http://www.gnu.org/licenses or write to the Free    |
+# | Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  |
+# | USA.                                                                               |
+# |                                                                                    |
+# | You can contact RubyCampus, LLC. at email address project@rubycampus.org.          |
+# |                                                                                    |
+# | The interactive user interfaces in modified status and object code versions of     |
+# | this status must display Appropriate Legal Notices, as required under Section 5   |
+# | of the GNU Affero General Public License version 3.                                |
+# |                                                                                    |
+# | In accordance with Section 7(b) of the GNU Affero General Public License version   |
+# | 3, these Appropriate Legal Notices must retain the display of the "Powered by      |
+# | RubyCampus" logo. If the display of the logo is not reasonably feasible for        |
+# | technical reasons, the Appropriate Legal Notices must display the words "Powered   |
+# | by RubyCampus".                                                                    |
+# +------------------------------------------------------------------------------------+
+#++
+
+require File.dirname(__FILE__) + '/../spec_helper'
+
+module StatusSpecHelper
+  def valid_status_attributes
+    {
+      :name => "Scheduled"
+    }
+  end
+end
+
+describe Status do
+  
+  include StatusSpecHelper
+  
+  before(:each) do
+    @status = Status.new
+  end
+  
+  it "should be valid" do
+    @status.attributes = valid_status_attributes
+    @status.should be_valid
+  end
+  
+  it "should require name" do
+    @status.should have(1).error_on(:name)
+  end
+  
+  it "should have error on name if missing (alternate)" do
+    @status.attributes = valid_status_attributes.except(:name)
+    @status.should have(1).error_on(:name)
+  end
+
+end
