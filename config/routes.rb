46ad5bfccf2dce2007a81570bdfd769f7117b978
Kevin Aleman <kaleman@rubycampus.org>
2008-10-30 16:48:35 +0900

    Adds enable and disable members to message_templates resources

--- config/routes.rb
+++ config/routes.rb
@@ -75,7 +75,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :head_of_households, :collection => { :lookup => :get }
   map.resources :location_types, :collection => { :lookup => :get, :index => :get }
   map.resources :marital_statuses, :collection => { :lookup => :get, :index => :get }
-  map.resources :message_templates, :collection => { :lookup => :get, :index => :get }
+  map.resources :message_templates, :collection => { :lookup => :get, :index => :get },
+                                    :member => { :enable => :put, :disable => :put }
   map.resources :messaging_providers, :collection => { :lookup => :get, :index => :get }
   map.resources :mobile_providers, :collection => { :lookup => :get, :index => :get }
   map.resources :name_prefixes, :collection => { :lookup => :get, :index => :get }
