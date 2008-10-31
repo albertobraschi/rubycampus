22f110a628576b15db8858d5af081aa46f14fd22
Kevin Aleman <kaleman@rubycampus.org>
2008-10-31 17:16:17 +0900

    Adds to contact types migration a description field

--- db/migrate/20080815204503_create_contact_types.rb
+++ db/migrate/20080815204503_create_contact_types.rb
@@ -39,6 +39,7 @@ class CreateContactTypes < ActiveRecord::Migration
   def self.up
     create_table :contact_types do |t|
       t.string     :name, :null => false
+      t.string     :description
       t.integer    :position
       t.boolean    :is_default, :default => false
       t.boolean    :is_reserved, :default => false
