dd39269242116522d855d70b50820b3b2ed72985
Kevin Aleman <kaleman@rubycampus.org>
2008-10-31 17:16:52 +0900

    Adds to genders migration a description field

--- db/migrate/20080815204507_create_genders.rb
+++ db/migrate/20080815204507_create_genders.rb
@@ -39,6 +39,7 @@ class CreateGenders < ActiveRecord::Migration
   def self.up
     create_table :genders do |t|
       t.string     :name, :null => false
+      t.string     :description
       t.integer    :position
       t.boolean    :is_default, :default => false
       t.boolean    :is_reserved, :default => false
