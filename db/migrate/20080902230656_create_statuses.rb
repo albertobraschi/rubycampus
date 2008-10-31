a9be39977f74e2998e139015d39ee5ba8bb93e21
Kevin Aleman <kaleman@rubycampus.org>
2008-10-31 17:19:34 +0900

    Adds to statuses migration a description field

--- db/migrate/20080902230656_create_statuses.rb
+++ db/migrate/20080902230656_create_statuses.rb
@@ -39,6 +39,7 @@ class CreateStatuses < ActiveRecord::Migration
   def self.up
     create_table :statuses do |t|
       t.string     :name, :null => false
+      t.string     :description
       t.integer    :position
       t.boolean    :is_default, :default => false
       t.boolean    :is_reserved, :default => false
