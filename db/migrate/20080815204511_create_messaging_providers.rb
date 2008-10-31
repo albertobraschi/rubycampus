d90c17307fdc3558e1746189c617750167482958
Kevin Aleman <kaleman@rubycampus.org>
2008-10-31 17:17:36 +0900

    Adds to messaging providers migration a description field

--- db/migrate/20080815204511_create_messaging_providers.rb
+++ db/migrate/20080815204511_create_messaging_providers.rb
@@ -39,6 +39,7 @@ class CreateMessagingProviders < ActiveRecord::Migration
   def self.up
     create_table :messaging_providers do |t|
       t.string     :name, :null => false
+      t.string     :description
       t.integer    :position
       t.boolean    :is_default, :default => false
       t.boolean    :is_reserved, :default => false
